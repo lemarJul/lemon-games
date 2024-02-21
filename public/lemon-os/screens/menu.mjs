e9f799c96c859e1a607f114551b918c4ad8751a2
Julien Lemarchand <lemar.jul@gmail.com>
2024-02-21 10:50:37 +0100

    feat: add custom attribute to target screen to display on click

--- public/lemon-os/screens/menu.mjs
+++ public/lemon-os/screens/menu.mjs
@@ -1,3 +1,16 @@
-export default function () {
+var redirectionAttribute = "show-screen";
 
+export default function () {
+  document
+    .querySelector("#display")
+    .addEventListener("click", redirectTo.bind(this));
+}
+
+function redirectTo({ target }) {
+  const redirection = target.getAttribute(redirectionAttribute);
+  if (redirection) {
+    console.log("redirecting to", redirection);
+    console.log("this.screenController.display", this.screenController.display);
+    this.screenController.display[redirection]();
+  }
 }
