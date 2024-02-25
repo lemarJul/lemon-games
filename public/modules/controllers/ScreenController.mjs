49efb47d7208e8886565c9d66b27b16fffda2092
Julien Lemarchand <lemar.jul@gmail.com>
2024-02-25 21:26:18 +0100

    refactor: move Screen module location and update import path

--- public/modules/controllers/ScreenController.mjs
+++ public/modules/controllers/ScreenController.mjs
@@ -1,6 +1,6 @@
 import AbstractController from "./AbstractController.mjs";
 import HTML from "../HtmlProvider.js";
-import Screen from "../Screen.mjs";
+import Screen from "../../components/Screen.mjs";
 
 export default class ScreenController extends AbstractController {
   constructor() {
