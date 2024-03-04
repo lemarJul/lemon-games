9848ca53f6af44477c97446ece7e1e4dde718c93
Julien Lemarchand <lemar.jul@gmail.com>
2024-03-04 18:52:34 +0100

    feat: update Minesweeper difficulty configurations

--- public/lemon-games/bootables/minesweeper/components/Grid/GridFactory.mjs
+++ public/lemon-games/bootables/minesweeper/components/Grid/GridFactory.mjs
@@ -2,22 +2,22 @@ import SquareGrid from "./SquareGrid.mjs"
 
 
 export default class GridFactory {
-  static _config = {
+  static difficulties = {
     // rows, cols, mines
-    test: { length: 5, nMines: 2 },
+    // test: { length: 5, nMines: 2 },
     easy: { length: 8, nMines: 10 },
-    medium: { length: 16, nMines: 40 },
-    hard: { length: 20, nMines: 75 },
+    medium: { length: 12, nMines: 25 },
+    hard: { length: 16, nMines: 50 },
   };
 
   static createSquareGrid(difficulty, safeCorners) {
-    if (!(difficulty in this._config)) {
-      const difficulties = Object.keys(this._config).join(", ");
+    if (!(difficulty in this.difficulties)) {
+      const difficulties = Object.keys(this.difficulties).join(", ");
       throw new Error(
         `Invalid difficulty "${difficulty}": must be one of ${difficulties}`
       );
     }
 
-    return new SquareGrid(this._config[difficulty], safeCorners);
+    return new SquareGrid(this.difficulties[difficulty], safeCorners);
   }
 }
