f795097a72b339776d43312000c376651ed64d51
Julien Lemarchand <lemar.jul@gmail.com>
2024-03-06 14:07:51 +0100

    feat: add new Class ScreenElementFactory

--- public/lemon-games/components/ScreenElementFactory.mjs
+++ public/lemon-games/components/ScreenElementFactory.mjs
@@ -1,3 +1,34 @@
+import ScreenElement from "./ScreenElementNEW.mjs";
+
 export default class ScreenElementFactory {
-    
+  constructor(manager) {
+    this.manager = manager;
+  }
+
+  async createScreenFromModule(module) {
+    const modulePath = module.path;
+    if (!modulePath) {
+      throw new Error("Module path not found");
+    }
+    const id =
+      module?.name || modulePath.split("/").pop().replace(".dynamizer.mjs", "");
+    //content
+    const contentPath = modulePath.replace(".dynamizer.mjs", ".html");
+    const content = await fetch(contentPath).then((response) =>
+      response.text()
+    );
+    //style
+    const stylePath = modulePath.replace(".dynamizer.mjs", ".css");
+    const link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = stylePath;
+    //script
+
+    const connectedCallback =
+      module.createConnectedCallback(this.manager) || (() => {});
+
+    const screen = new ScreenElement(id, content, link, connectedCallback);
+    console.log("screen: " + screen);
+    return screen;
+  }
 }
