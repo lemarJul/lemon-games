def047c262d549b5a74bbc99a83e838b4a23f28a
Julien Lemarchand <lemar.jul@gmail.com>
2024-03-07 17:44:28 +0100

    feat: add index.mjs as a barrel for module imports
    This commit adds a new index.mjs file that re-exports all modules in the same directory. This simplifies imports elsewhere in the application by providing a single point of access to these modules.

---
+++ public/lemon-games/bootables/minesweeper/screens/index.mjs
@@ -0,0 +1,7 @@
+export * as menuScreen from "./menu.dynamizer.mjs";
+export * as gridScreen from "./grid.dynamizer.mjs";
+export * as failScreen from "./fail.dynamizer.mjs";
+export * as winScreen from "./win.dynamizer.mjs";
+export * as settingsScreen from "./settings.dynamizer.mjs";
+export * as leaderboardScreen from "./leaderboard.dynamizer.mjs";
+export * as pausedScreen from "./paused.dynamizer.mjs";
