372f150b2c658b75474da824a928b0ffff97a4a4
Julien Lemarchand <lemar.jul@gmail.com>
2024-03-11 08:45:26 +0100

    fix: update 'this' by screen var

--- public/lemon-games/bootables/minesweeper/screens/win.dynamizer.mjs
+++ public/lemon-games/bootables/minesweeper/screens/win.dynamizer.mjs
@@ -1,76 +1,5 @@
 //TODO: display score position in the page
 
-export const createConnectedCallback = (manager) => {
-  return function () {
-    const nameInput = this.querySelector("#name-submit"),
-      form = this.querySelector("form"),
-      timeDisplay = this.querySelector("#time-result");
-
-    // update the time display
-    const displayTimer = () => {
-      const time = manager.timer.getTime();
-      timeDisplay.textContent = time;
-    };
-
-    const displayForm = () => {
-      form.removeAttribute("hidden");
-    };
-
-    const hideSubmitMessages = () => {
-      this.querySelectorAll(".submit-message").forEach((el) =>
-        el.setAttribute("hidden", "")
-      );
-    };
-
-    new IntersectionObserver(
-      function (entries) {
-        if (entries[0].isIntersecting === true) {
-          displayTimer();
-          displayForm();
-          hideSubmitMessages();
-          form.reset();
-        }
-      },
-      { threshold: [0] }
-    ).observe(this);
-
-    checkEmptyState({ target: nameInput });
-    nameInput.addEventListener("input", checkEmptyState);
-
-    function checkEmptyState({ target }) {
-      if (target.value.length > 0) {
-        target.classList.remove("empty");
-      } else {
-        target.classList.add("empty");
-      }
-    }
-
-    // submit the form
-    form.addEventListener("submit", async (event) => {
-      event.preventDefault();
-
-      const name = nameInput.value;
-      const score = manager.timer.getTime().toString();
-      const difficulty = manager.HTMLElements.difficulty.value;
-      try {
-        const res = await manager.dataController.setData({
-          name,
-          score,
-          difficulty,
-        });
-        alert("Score saved!");
-        form.reset();
-        form.setAttribute("hidden", "");
-        this.querySelector("#success-message").removeAttribute("hidden");
-      } catch (error) {
-        console.error(error);
-      }
-    });
-  };
-};
-
-export const path = import.meta.url;
-
 export default async (manager) => {
   const screen = await manager.screenElementFactory.createScreenFromPath(
     import.meta.url,
@@ -103,9 +32,9 @@ export default async (manager) => {
   }
 
   function hideSubmitMessages() {
-    this.querySelectorAll(".submit-message").forEach((el) =>
-      el.setAttribute("hidden", "")
-    );
+    screen
+      .querySelectorAll(".submit-message")
+      .forEach((el) => el.setAttribute("hidden", ""));
   }
   function initNameInput() {
     checkEmptyState({ target: nameInput });
@@ -136,7 +65,7 @@ export default async (manager) => {
         alert("Score saved!");
         form.reset();
         form.setAttribute("hidden", "");
-        this.querySelector("#success-message").removeAttribute("hidden");
+        screen.querySelector("#success-message").removeAttribute("hidden");
       } catch (error) {
         console.error(error);
       }
