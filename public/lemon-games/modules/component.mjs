37f1e20e4ae80bfa1bec99498a9f13502fca951d
Julien Lemarchand <lemar.jul@gmail.com>
2024-07-25 20:59:03 +0200

    feat(component.mjs): update fetchHtml and fetchStyle methods

--- public/lemon-games/modules/component.mjs
+++ public/lemon-games/modules/component.mjs
@@ -7,20 +7,35 @@ export class Component {
   }
 
   static async fetchHtml(path) {
-    return fetch(path.replace(/\.mjs$/, ".html")).then((response) =>
-      response.text()
-    );
+    return fetch(path.replace(/\.mjs$/, ".html"))
+      .then((response) => response.text())
+      .then((text) => {
+        console.log("text", text);
+        return Component.parseHTML(text);
+      });
   }
 
-  static async fetchCss(path) {
-    return fetch(path.replace(/\.mjs$/, ".css")).then((response) =>
-      response.text()
+  static async fetchStyle(path) {
+    const style = document.createElement("style");
+    style.textContent = await fetch(path.replace(/\.mjs$/, ".css")).then(
+      (response) => response.text()
     );
+    return style;
   }
 
-  static async fetchHtmlCss(path) {
+  static async fetchHtmlAndStyle(path) {
     const html = await Component.fetchHtml(path);
-    const css = await Component.fetchCss(path);
+    const css = await Component.fetchStyle(path);
     return { html, css };
   }
+  static parseHTML(htmlString) {
+    return document.createRange().createContextualFragment(htmlString)
+      .firstElementChild;
+  }
+  // static render(component) {
+  //   if (!component.template)
+  //     throw Error(`·Missing template property in component ${component.name}`);
+  //   component.innerHtml = "";
+  //   component.appendChild(Component.parseHTML(component.template).content);
+  // }
 }
