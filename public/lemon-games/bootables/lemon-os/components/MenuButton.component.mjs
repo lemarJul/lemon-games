67df596b0ef26d4df0390f18b325626bf447c447
Julien Lemarchand <lemar.jul@gmail.com>
2024-07-26 22:29:13 +0200

    refactor: move ChildDisplayManager component to a new location

--- public/lemon-games/bootables/lemon-os/components/MenuButton.component.mjs
+++ public/lemon-games/bootables/lemon-os/components/MenuButton.component.mjs
@@ -1,6 +1,6 @@
 import { Component } from "../../../modules/Component.mjs";
 import { canLinkLocalStyle } from "../../../mixins/componentMixins.mjs";
-import ScreenController from "../../../controllers/ChildDisplayManager.component.mjs";
+import ScreenController from "../../../components/ChildDisplayManager.component.mjs";
 
 export default Component.define(
   "menu-button",
