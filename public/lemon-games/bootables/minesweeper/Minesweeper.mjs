61466da70cb32e18ad04423b9b1fb95d97cfe4ed
Julien Lemarchand <lemar.jul@gmail.com>
2024-07-26 14:24:58 +0200

    import PausedScreen component

--- public/lemon-games/bootables/minesweeper/Minesweeper.mjs
+++ public/lemon-games/bootables/minesweeper/Minesweeper.mjs
@@ -2,6 +2,8 @@ import AbstractExe from "../AbstractExe.mjs";
 import DescendantsProvider from "./modules/DescendantsProvider.mjs";
 
 import * as screenComponents from "./screens/index.mjs";
+import "./components/index.mjs";
+
 export default class MineSweeperManager extends AbstractExe {
   constructor({ screenController, buttonController, soundController }) {
     super({
